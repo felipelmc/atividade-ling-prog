26b7d2f98b76402e812a316cdaa9ff352b90ca00
Bianca Carvalho <bianca.dias1025@gmail.com>
2021-10-27 20:05:55 -0300

    biaapp

--- atividade_ling_prog/biaapp/views.py
+++ atividade_ling_prog/biaapp/views.py
@@ -1,3 +1,6 @@
+from django.http import request
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<strong>Bianca</strong>')
