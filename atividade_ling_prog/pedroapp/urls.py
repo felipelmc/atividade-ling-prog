c8993bc93b6e1a0768b09246f33225c4f52e819e
PedroGarcia6 <limagarciapedro770@gmail.com>
2021-10-27 19:51:39 -0400

    adicionando algo

--- atividade_ling_prog/pedroapp/urls.py
+++ atividade_ling_prog/pedroapp/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls.conf import include
+from pedroapp import views as pedroapp_views 
 
 urlpatterns = [
-    path(r'pedro/', include('pedroapp.urls')),
+    path('', pedroapp_views.index, name='teste')
 ]
