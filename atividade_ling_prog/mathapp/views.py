bc7fd1e98497694166b52a192d3d3d9837ccf133
MatheusMCF <mmcf2001@yahoo.com.br>
2021-10-27 21:57:42 -0300

    página web e afins

--- atividade_ling_prog/mathapp/views.py
+++ atividade_ling_prog/mathapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('<strong>Teste do Matheus</strong>')
+
+def matheus(request):
+    return render(request, 'matheus.html')
