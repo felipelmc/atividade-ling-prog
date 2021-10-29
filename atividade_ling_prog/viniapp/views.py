7ac90705489f852e7a5eea33087a4dc1513e30d5
Vinicius Hedler <viniciushedler@gmail.com>
2021-10-29 07:24:42 -0300

    vini

--- atividade_ling_prog/viniapp/views.py
+++ atividade_ling_prog/viniapp/views.py
@@ -4,3 +4,6 @@ from django.http.response import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<strong> Vini aqui </strong>")
+
+def special(request):
+    return render(request, "vini/")
