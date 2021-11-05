2264b6637e497ce5f3b3645ffdfd28ca3be1dc53
Felipe Lamarca <felipelmc@pop-os.localdomain>
2021-11-05 09:16:16 -0300

    Atualizações aula 05/11 felipeapp

--- atividade_ling_prog/felipeapp/urls.py
+++ atividade_ling_prog/felipeapp/urls.py
@@ -4,8 +4,12 @@ from felipeapp import views as felipeapp_views
 
 urlpatterns = [
     path('', felipeapp_views.index, name='teste'),
-    path('redes-sociais/', felipeapp_views.pag1, name='felipeapp'),
-    path('musicas/', felipeapp_views.pag2, name='felipeapp'),
-    path('faculdade/', felipeapp_views.pag3, name='felipeapp'),
+    #path('redes-sociais/', felipeapp_views.pag1, name='felipeapp'),
+    #path('musicas/', felipeapp_views.pag2, name='felipeapp'),
+    #path('faculdade/', felipeapp_views.pag3, name='felipeapp'),
+    path('special_str/<str:param>/', felipeapp_views.view_dinamica_str, name = 'dinamica-str'),
+    path('special_int/<int:param>/', felipeapp_views.view_dinamica_int, name = 'dinamica-int'),
+    path('redireciona/', felipeapp_views.redireciona, name='redireciona'),
+    path('dtl/', felipeapp_views.special_dtl, name='retorna-html-dtl')
 ]
 
--- atividade_ling_prog/felipeapp/views.py
+++ atividade_ling_prog/felipeapp/views.py
@@ -1,14 +1,46 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse 
 
 def index(request):
     return HttpResponse('<strong>Teste do Felipe</strong>')
 
-def pag1(request):
-    return render(request, 'felipeapp/redes-sociais.html')
+#def pag1(request):
+    #return render(request, 'felipeapp/redes-sociais.html')
 
-def pag2(request):
-    return render(request, 'felipeapp/musicas.html')
+# def pag2(request):
+    # return render(request, 'felipeapp/musicas.html')
 
-def pag3(request):
-    return render(request, 'felipeapp/faculdade.html')
+# def pag3(request):
+    # return render(request, 'felipeapp/faculdade.html')
+
+def view_dinamica_int(request, param):
+    if param == 1:
+        return HttpResponse('<strong>Parâmetro 1</strong>')
+    elif param == 2:
+        return HttpResponse('<strong>Parâmetro 2</strong>')
+    elif param == 3:
+        return HttpResponse('<strong>Parâmetro 3</strong>')
+    else:
+        return HttpResponseNotFound("Página não existe!")
+    
+def view_dinamica_str(request, param):
+    if param == 'redes-sociais':
+        return render(request, 'felipeapp/redes-sociais.html')
+    elif param == 'musicas':
+        return render(request, 'felipeapp/musicas.html')
+    elif param == 'faculdade':
+        return render(request, 'felipeapp/faculdade.html')
+    else:
+        return HttpResponseNotFound("Página não existe!")
+    
+def redireciona(request):
+    url_redirecionamento = reverse('dinamica-str', args=['redes-sociais'])
+    return HttpResponseRedirect(url_redirecionamento) 
+
+def special_dtl(request):
+    context = {
+        'nome':'Amiguinho',
+        'nome_familia':'Lamarca'
+    }
+    return render(request, 'felipeapp/felipeapp-dtl.html', context)
