0002c5595693e3eacc6e920926c14489f21d0ce8
Sofia <sofia.laks15@gmail.com>
2021-11-10 08:20:09 -0300

    adicionei o rodape

--- atividade_ling_prog/sofiaapp/urls.py
+++ atividade_ling_prog/sofiaapp/urls.py
@@ -3,7 +3,11 @@ from sofiaapp import views as views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sofiaapp/', views.sofia, name='sofiaapp'),
-    path('laks/', views.laks, name='sofiaapp'),
-    path('vitz/', views.vitz, name='sofiaapp'),
+    #path('sofiaapp/', views.sofia, name='sofiaapp'),
+    #path('laks/', views.laks, name='sofiaapp'),
+    #path('vitz/', views.vitz, name='sofiaapp'),
+    path('special_str/<str:param>/', views.view_dinamica_str, name = 'dinamica-str'),
+    path('special_int/<int:param>/', views.view_dinamica_int, name = 'dinamica-int'),
+    path('redireciona/', views.redireciona, name='redireciona'),
+    path('dtl/', views.special_dtl, name='retorna-html-dtl'),
 ]
